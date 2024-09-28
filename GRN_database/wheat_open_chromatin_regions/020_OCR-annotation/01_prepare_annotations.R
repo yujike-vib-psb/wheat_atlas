c871724bf8b0a347b7684ca2f15220b84a2bb619
yujike-vib-psb <135733149+yujike-vib-psb@users.noreply.github.com>
2024-09-28 15:00:15 +0200

    Update 01_prepare_annotations.R

--- GRN_database/wheat_open_chromatin_regions/020_OCR-annotation/01_prepare_annotations.R
+++ GRN_database/wheat_open_chromatin_regions/020_OCR-annotation/01_prepare_annotations.R
@@ -1,1 +1,50 @@
+# ############################################################################ #
+# Prepare genome annotations for OCRs
+# ############################################################################ #
 
+# ============================================================================ #
+# Setup                                                                   ====
+# ============================================================================ #
+
+## Load libraries
+library(here)
+here::i_am(".here")
+
+library(tidyverse)
+library(glue)
+
+# GenomicFeatures and AnnotationDbi (mask too many standard functions)
+
+## --------------------------------------------------------------------------- #
+## Define inputs/outputs                                                  ----
+## --------------------------------------------------------------------------- #
+
+### Paths
+paths      <- list()
+paths$core <- here("data_core")
+paths$out  <- here("data_processed/020_OCR-annotation")
+
+### General
+general <- list(
+  # Genome annotations (GFF3)
+  annotations = glue("{paths$core}/triticum-aestivum_IWGSC-2.1_simplified.gff3.gz"),
+  # Outputs
+  out_anno    = glue("{paths$out}/triticum-aestivum_IWGSC-2.1_simplified.sqlite")
+)
+
+# ============================================================================ #
+# Process                                                                 ====
+# ============================================================================ #
+
+## Create output directory
+if(!dir.exists(paths$out)) dir.create(paths$out, recursive = TRUE)
+
+## Convert with GenomicFeatures
+annotations <- GenomicFeatures::makeTxDbFromGFF(
+  file       = general$annotations,
+  dataSource = "IWGSC",
+  organism   = "Triticum aestivum"
+)
+
+## Export
+annotations |> AnnotationDbi::saveDb(general$out_anno)
